49dea3a4f31a5f32b4c0a1718e45108d0559c7f3
Unknown <whdydql123@naver.com>
2018-02-01 15:51:04 +0900

    PEP8에 맞추어 INDENT 처리
    Pycharm pep8 가이드에 맞추어 들여쓰기 처리 tab에서 4 spaces로 처리 하고 함수간 간격 조정 함

--- app.py
+++ app.py
@@ -5,28 +5,33 @@ from entities.user  import User
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def _index():
-    return redirect(url_for('branch_form'));
+def index():
+    return redirect(url_for('branch_form'))
+
 
 @app.route("/branch_form/", methods=[ 'GET','POST'])
 def branch_form():
-	if request.method == 'POST':
-		object = User(username=request.form['userName'], number=request.form['phone1'])
-		db.session.add(object)
-		db.session.commit()
+    if request.method == 'POST':
+        _object = User(username=request.form['userName'], number=request.form['phone1'])
+        db.session.add(_object)
+        db.session.commit()
 
-		return render_template("branchform.html")
-	else:
-		return render_template("branchform.html")
+        return render_template("branchform.html")
+    else:
+        return render_template("branchform.html")
+
 
 @app.route("/test1/")
 def test1():
-	return render_template("extends_test1.html")
+    return render_template("extends_test1.html")
+
 
 @app.route("/test2/")
 def test2():
-	return render_template("extends_test2.html")
+    return render_template("extends_test2.html")
+
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
--- entities/user.py
+++ entities/user.py
@@ -2,10 +2,10 @@ from database import db
 
 
 class User(db.Model):
-	__tablename__ = 'TBL_TEST'
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(80), nullable=False)
-	number = db.Column(db.Integer, nullable=False)
+    __tablename__ = 'TBL_TEST'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    number = db.Column(db.Integer, nullable=False)
 
-	def __repr__(self):
-		return '<User %r>' %self.username
+    def __repr__(self):
+        return '<User %r>' % self.username
