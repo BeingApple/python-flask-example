6f18ac6535fc5a443d09b409f557792e7bb01043
Unknown <whdydql123@naver.com>
2018-02-01 17:23:59 +0900

    app.py branchform 중복 제거, PEP8 적용
    methods=[ 'GET', 'POST']) 의 쉼표 뒤 띄어쓰기 추가 
    if else 문 내의 중복 구문 제거 하고 변경

--- app.py
+++ app.py
@@ -15,16 +15,13 @@ def index():
     return redirect(url_for('branch_form'))
 
 
-@app.route("/branch_form/", methods=[ 'GET','POST'])
+@app.route("/branch_form/", methods=[ 'GET', 'POST'])
 def branch_form():
     if request.method == 'POST':
         _object = User(username=request.form['userName'], number=request.form['phone1'])
         db.session.add(_object)
         db.session.commit()
-
-        return render_template("branchform.html")
-    else:
-        return render_template("branchform.html")
+    return render_template("branchform.html")
 
 
 @app.route("/test1/")
@@ -34,6 +31,7 @@ def test1():
 
 @app.route("/test2/")
 def test2():
+
     return render_template("extends_test2.html")
 
 
