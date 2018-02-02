ad550e2a54320c727cae9e43d577b7251cdbf4ed
Unknown <whdydql123@naver.com>
2018-02-02 18:32:29 +0900

    pytest branch_form post 전송에 대한 단위 테스트 추가
    branch_form 에 post 데이터 전송하여 정상 응답 코드가 돌아오는지 확인하기 위한 단위 테스트 코드 추가

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,6 +6,12 @@ def test_branch_form(flask_client):
     assert resp.status_code == 200
 
 
+def test_branch_form_post(flask_client):
+    data = {'userName' : 'TEST-USER-NAME', 'phone1' : '123456'}
+    resp = flask_client.post('/branch_form/', data=data)
+    assert resp.status_code == 200
+
+
 def test_test1(flask_client):
     resp = flask_client.get('/test1/')
     assert resp.status_code == 200
