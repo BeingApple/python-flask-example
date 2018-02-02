f70ba5b765beff6b5f320e83fed1f71f983f345c
Unknown <whdydql123@naver.com>
2018-02-02 16:06:38 +0900

    EC2 RDS에서 heroku JAWS DB로 변경처리
    기존 EC2 RDS MYSQL에서 heroku 애드온의 JAWS DB MYSQL로 데이터베이스 연결 정보 변경

--- config/database_config.py
+++ config/database_config.py
@@ -1,6 +1,5 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://jaljal:jaljal!!##@' \
-                          'test-recipejaljal' \
-                          '.clyhyylconee.ap-northeast-2' \
-                          '.rds.amazonaws.com:3306/JALJAL'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://rjuewudbxxnxbyqk:xhjtifutx6j8yeew@' \
+                          'fugfonv8odxxolj8.cbetxkdyhwsb.' \
+                          'us-east-1.rds.amazonaws.com:3306/ecj4dxolnh4i4v2q'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
